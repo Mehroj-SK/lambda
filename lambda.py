e0c808c5a9be650855b7397598a00990000ab64d
Mehroj-SK <115388393+Mehroj-SK@users.noreply.github.com>
2022-12-08 22:44:48 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,28 @@
+
+# from functools import reduce 
+
+# list1 = [3, 7, 13, 22, 34, 48, 51]
+
+# output = list(map(lambda root : root**2,list1))
+
+# output2 = list(map(lambda i : i + 2, list1))
+
+# output3 = reduce(lambda a,b:a+b,list1) 
+
+# print(output3) 
+
+
+
+# list1 = [10,20,30,40,50]
+# triple all the elements in the list 
+
+# list1 = [10,20,30,40,50]
+# output = list(map(lambda i : i*3, list1))
+# print(output) 
+
+# list2 = [34,88,30,22,10,15,18]
+# print out all the multiples of 5 using filter and lambda 
+
+# list2 = [34,88,30,22,10,15,18]
+# output = list(filter(lambda i : i%5==0,list2))
+# print(output) 
